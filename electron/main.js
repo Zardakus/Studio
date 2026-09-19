081b973e9f1a0ed27c8d1fc439e662db079d6be4
Pedro Paulo <pedroonlinerm@hotmail.com>
2026-09-19 18:38:21 -0300

    chore: migrate web MVP to desktop using Electron (Ref: #6)

---
+++ electron/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Remove menu bar for app-like feel
+  mainWindow.setMenuBarVisibility(false);
+
+  // In production, load the static build. In dev, load localhost.
+  const isDev = !app.isPackaged;
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ electron/preload.js
@@ -0,0 +1,4 @@
+// preload.js
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Alkemion Studio Desktop App Loaded');
+});
